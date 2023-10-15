e75693f7bd87857ef82f483cd1f4e9798a0620a8
sunnysood12 <106012686+sunnysood12@users.noreply.github.com>
2023-10-15 20:31:58 +0530

    Create Exp3.py

---
+++ exp3.py
@@ -0,0 +1,15 @@
+# Sample list
+my_list = [1, 2, 3, 4, 5]
+
+# Iterating over the list using a for loop
+print("Iterating over a list:")
+for item in my_list:
+    print(item)
+
+# Sample dictionary
+my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+
+# Iterating over the dictionary using a for loop to print keys and values
+print("\nIterating over a dictionary:")
+for key, value in my_dict.items():
+    print(key, ":", value)
